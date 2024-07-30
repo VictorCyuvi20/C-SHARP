d93ab284016b626f9a083994f507ffdbf3d19e4b
VictorCyuvi20 <victoramancio2020@gmail.com>
2024-07-30 14:53:12 -0300

    PRIMEIRO CÓDIGO C#

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace C_SHARP;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("******************************");
+        Console.WriteLine("MEU PRIMEIRO PROGRAMA EM C#");
+        Console.WriteLine("******************************");
+        Console.WriteLine("Qual o seu nome? ");
+        string nome = Console.ReadLine()!;
+        Console.WriteLine("bom dia "+ nome + ", seja bem-vindo!!");
+        Console.WriteLine("Quantos anos você tem?");
+        int idade = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Você tem " + idade + " anos");                             
+        if (idade > 17)
+            Console.WriteLine("VOCÊ PODE SER PRESO!!");                   
+        else 
+            Console.WriteLine("VOCÊ VAI PRA FEBEM");
+    }
+}
---
+++ verdades.py
@@ -0,0 +1,6 @@
+resposta = 0
+pergunta = float(("QUEM GANHA: GOKU VS SAITAMA"))
+if resposta == "goku":
+    print("PARABENS VC NÃO É BURRO")
+else:
+    print("VOCE É BURRO PRA KRL KKKKKKKKKKKKKKKKKKKKKKK")
